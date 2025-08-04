d54af49105d36f846a4d3ff540824c715158a6e2
NstecEmp-024 <nstecemp_024@fullness.email>
2025-08-04 09:05:15 +0900

    Initial commit

---
+++ src/main/java/com/example/githubflow/GithubflowApplication.java
@@ -0,0 +1,13 @@
+package com.example.githubflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubflowApplication.class, args);
+	}
+
+}

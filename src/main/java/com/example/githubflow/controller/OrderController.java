3df3f83a1fafc2833fd40337281fe488423c83b1
NstecEmp-008 <nstecemp_008@fullness.email>
2025-08-04 09:39:27 +0900

    実験

--- src/main/java/com/example/githubflow/controller/OrderController.java
+++ src/main/java/com/example/githubflow/controller/OrderController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class OrderController {
   @GetMapping("/order")
   public String hello() {
+    System.out.println("OrderController.hello() called");
+    // Simulating a delay to mimic processing time
     return "order feature!";
   }
 }
